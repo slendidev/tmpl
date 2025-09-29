29e39f17e9ae9131f07ba827271a5e68707b97fd
Slendi <slendi@socopon.com>
2025-09-29 14:27:19 +0300

    stuffg
    Signed-off-by: Slendi <slendi@socopon.com>

---
+++ odin/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "My flake";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            odin
+            ols
+            lldb
+          ];
+        };
+      }
+    );
+}
