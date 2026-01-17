5b70efa6b234e6109817117f573f34e6e2f700e4
Slendi <slendi@socopon.com>
2026-01-17 15:53:26 +0200

    Remove flakehub
    Signed-off-by: Slendi <slendi@socopon.com>

--- php/flake.nix
+++ php/flake.nix
@@ -2,7 +2,7 @@
   description = "My flake";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
--- rust/flake.nix
+++ rust/flake.nix
@@ -2,7 +2,7 @@
   description = "Rust flake";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
